23550ef1d6d8a6ac74b3dfa8a605140fa2ed5f13
Fitz <142354039+fitzinn@users.noreply.github.com>
2024-04-12 11:14:56 -0300

    Oracle Cloud
    Mudança na conexão do banco de dados

--- BancodeDados/banco_de_dados.py
+++ BancodeDados/banco_de_dados.py
@@ -2,12 +2,13 @@ import oracledb
 
 # Conectar ao banco de dados
 def conectar_bd():
-    connection = oracledb.connect(
-        user="BD150224513", 
-        password="Opxuf5", 
-        host="172.16.12.14", 
-        port="1521"
-        )
+    connection=oracledb.connect(
+        config_dir=r"C:\OracleCloud\Wallet\Wallet_ProjetoIntegrador1.zip",
+        user="admin",
+        password="H7$e/ZbX@##s*.q",
+        dsn="db2024_low",
+        wallet_location=r"C:\OracleCloud\Wallet\Wallet_ProjetoIntegrador1.zip",
+        wallet_password="H7$e/ZbX@##s*.q")
     return connection
 
 connection = conectar_bd()
