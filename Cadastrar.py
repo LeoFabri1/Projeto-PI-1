2facc901bf9ec39b2ef9bd402910351e7207b2b7
Fitz <142354039+fitzinn@users.noreply.github.com>
2024-05-14 15:29:46 -0300

    Cadastro

--- Cadastrar.py
+++ Cadastrar.py
@@ -5,7 +5,6 @@ def cadastrar_prato():
     print("****************************************************************************")
     print("Cadastro de Pratos")
     #pega os dados
-    id_prato = int(input("Digite o ID do prato: "))
     nome_prato = str(input("Digite o nome do prato: "))
     desc_prato = str(input("Digite a descrição do prato: "))
     categoria_prato = str(input("Digite a categoria do prato: "))
@@ -18,14 +17,14 @@ def cadastrar_prato():
     connection = conectar_bd()
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO Pratos (id_prato, nome_prato, desc_prato, categoria, custo_prato) VALUES (:1, :2, :3, :4, :5)', (id_prato, nome_prato, desc_prato_cripto, categoria_prato, custo_prato))
+    cursor.execute('INSERT INTO Pratos (id_prato, nome_prato, desc_prato, categoria, custo_prato) VALUES (seq_id_add.NEXTVAL, :1, :2, :3, :4)', (nome_prato, desc_prato_cripto, categoria_prato, custo_prato))
     print("Cadastrado com sucesso!")
 
     connection.commit()
     connection.close()
 
 #teste
-cadastrar_prato()
+#cadastrar_prato()
 #id = 2
 #nome = Bibimbap
 #desc = Uma tigela vibrante de arroz coberta com uma variedade de vegetais frescos, carne grelhada, ovo frito e gochujang. Misture todos os ingredientes para uma explosao de sabores e texturas. Uma refeicao coreana classica que equilibra perfeitamente o doce, o picante e o salgado em cada garfada.
@@ -36,7 +35,6 @@ def cadastrar_fab():
     print("****************************************************************************")
     print("Fabricante")
     #pega os dados
-    id_fab = int(input("Digite o id do fabricante: "))
     nome_fab = str(input("Digite o nome do fabricante: "))
     tel_fab = int(input("Digite o telefone do fabricante: "))
     email_fab = str(input("Digite o email do fabricante: "))
@@ -45,17 +43,19 @@ def cadastrar_fab():
     connection = conectar_bd()
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO Fabricantes (id_fab, nome_fab, tel_fab, email_fab) VALUES (:1, :2, :3, :4)', (id_fab, nome_fab, tel_fab, email_fab))
+    cursor.execute('INSERT INTO Fabricantes (id_fab, nome_fab, tel_fab, email_fab) VALUES (seq_id_add.NEXTVAL, :1, :2, :3)', (nome_fab, tel_fab, email_fab))
     print("Cadastrado com sucesso!")
 
     connection.commit()
     connection.close()
 
+#teste
+#cadastrar_fab()
+
 def cadastrar_forn():
     print("****************************************************************************")
     print("Fornecedor")
     #pega os dados
-    id_forn = int(input("Digite o id do fornecedor: "))
     nome_forn = str(input("Digite o nome do fornecedor: "))
     tel_forn = int(input("Digite o telefone do fornecedor: "))
     email_forn = str(input("Digite o email do fornecedor: "))
@@ -63,18 +63,19 @@ def cadastrar_forn():
     connection = conectar_bd()
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO Fornecedores (id_forn, nome_forn, tel_forn, email_forn) VALUES (:1, :2, :3, :4)', (id_forn, nome_forn, tel_forn, email_forn))
+    cursor.execute('INSERT INTO Fornecedores (id_forn, nome_forn, tel_forn, email_forn) VALUES (seq_id_add.NEXTVAL, :1, :2, :3)', (nome_forn, tel_forn, email_forn))
     print("Cadastrado com sucesso!")
 
     connection.commit()
     connection.close()
 
+#teste
+#cadastrar_forn()
+
 def cadastrar_func():
     print("****************************************************************************")
     print("Funcionario")
-    aux_id_func = 0
     #pega os dados
-    id_func = int(input("Digite o id do funcionario: "))
     nome_func = str(input("Digite o nome do funcionario: "))
     data_nasc_func = int(input("Digite a data de nascimento do funcionario: "))
     sal_func = float(input("Digite o salario do funcionario: "))
@@ -85,16 +86,15 @@ def cadastrar_func():
     connection = conectar_bd()
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO Funcionarios (id_func, nome_func, data_nasc_func, sal_func, data_contrato, categoria_func) VALUES (:1, :2, :3, :4, :5, :6)', (id_func, nome_func, data_nasc_func, sal_func, data_contrato, categoria_func))
+    cursor.execute('INSERT INTO Funcionarios (id_func, nome_func, data_nasc_func, sal_func, data_contrato, categoria_func) VALUES (seq_id_add.NEXTVAL, :1, :2, :3, :4, :5)', (nome_func, data_nasc_func, sal_func, data_contrato, categoria_func))
     print("Cadastrado com sucesso!")
 
     connection.commit()
     connection.close()
 
-    id_login = int(input("Digite o id de login do funcionario: "))
     email_login = str(input("Digite o email de login do funcionario: "))
     senha_login = str(input("Digite a senha de login do funcionario: "))
-    id_func = aux_id_func
+    id_func = int(input("Digite o id do funcionário do login a ser cadastrado:"))
 
     #senha funcionario criptografada
     senha_func_cripto = cripto(senha_login)
@@ -103,18 +103,19 @@ def cadastrar_func():
     connection = conectar_bd()
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO Login_Func (id_login, email_login, senha_login, id_func) VALUES (:1, :2, :3, :4)', (id_login, email_login, senha_login, id_func))
+    cursor.execute('INSERT INTO Login_Func (id_login, email_login, senha_login, id_func) VALUES (seq_id_add.NEXTVAL, :1, :2, :3)', (email_login, senha_func_cripto, id_func))
     print("Cadastrado com sucesso!")
 
     connection.commit()
     connection.close()
 
+#teste
+#cadastrar_func()
 
 def cadastrar_cliente():
     print("****************************************************************************")
     print("Usuario")
     #pega os dados
-    id_cliente = int(input("Digite o id de login do cliente: "))
     nome_cliente = str(input("Digite o nome de login do cliente: "))
     email_cliente = str(input("Digite o email de login do cliente: "))
     senha_cliente = str(input("Digite a senha de login do cliente: "))
@@ -126,27 +127,33 @@ def cadastrar_cliente():
     connection = conectar_bd()
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO Clientes (id_cliente, nome_cliente, email_cliente, senha_cliente) VALUES (:1, :2, :3, :4)', (id_cliente, nome_cliente, email_cliente, senha_cliente))
+    cursor.execute('INSERT INTO Login_Clientes (id_cliente, nome_cliente, email_cliente, senha_cliente) VALUES (seq_cliente_id.NEXTVAL, :1, :2, :3)', (nome_cliente, email_cliente, senha_cliente_cripto))
     print("Cadastrado com sucesso!")
 
     connection.commit()
     connection.close()
+
+#teste
+#cadastrar_cliente()
 
 #adicionar adm
 def cadastrar_perm():
     print("****************************************************************************")
     print("Permissoes")
     #pega os dados
-    id_adm = int(input("Digite o id do administrador: "))
-    usuario_adm = str(input("Digite o usuario do administrador: "))
+    id_func = int(input("Digite o id do funcionario a virar administrador: "))
+    email_adm = str(input("Digite o email do administrador: "))
     senha_adm = str(input("Digite a senha do administrador: "))
 
     #adiciona ao banco
     connection = conectar_bd()
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO Login_ADM (id_adm, usuario_adm, senha_adm) VALUES (:1, :2, :3)', (id_adm, usuario_adm, senha_adm))
+    cursor.execute('INSERT INTO Login_ADM (id_adm, email_adm, senha_adm, id_func) VALUES (seq_id_add.NEXTVAL, :1, :2, :3)', (email_adm, senha_adm, id_func))
     print("Cadastrado com sucesso!")
 
     connection.commit()
     connection.close()
+
+#teste
+#cadastrar_perm()
